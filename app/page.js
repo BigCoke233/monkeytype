b059d059b5a60ae885ea81bb974c16e54eac4ed1
Eltrac <hi@guhub.cn>
2023-12-18 00:34:36 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -26,7 +26,9 @@ export default function Home() {
         <header className="flex items-center gap-3">
           <Image src="/favicon.png" alt="A monkey typist." width={96} height={96}
             className="w-8 md:w-12 drop-shadow-md" />
-          <h1 className={`texl-xl md:text-3xl font-bold drop-shadow ${font.className}`}>Monkeytype</h1>
+          <h1 className={`texl-xl md:text-3xl font-bold drop-shadow ${font.className}`}>
+            Monkeytype<span className="text-zinc-600">_</span>
+          </h1>
         </header>
         <section>
           <Button isIconOnly variant="light" size="lg"><IoSettings /></Button>
