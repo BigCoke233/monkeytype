7871574a23a5997d8f2d78ce74c1ef328cfd7306
Eltrac <hi@guhub.cn>
2023-12-21 00:12:07 +0800

    fix: 部分 Tailwind 类失效

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="zh-cn" className='dark'>
       <body className='bg-gradient-to-b from-zinc-700 to-zinc-800 min-h-screen'>
+          <div id="tailwindInitializer" className='text-lg border-b border-zinc-600 py-2' />
           <div className='monkey' />
           {children}
       </body>
