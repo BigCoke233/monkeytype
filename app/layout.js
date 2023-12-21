a1e353ce476d8c0409af19828ba26be353a0f77a
Eltrac <hi@guhub.cn>
2023-12-21 15:50:12 +0800

    feat: 添加删除历史记录功能

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="zh-cn" className='dark'>
       <body className='bg-gradient-to-b from-zinc-700 to-zinc-800 min-h-screen'>
-          <div id="tailwindInitializer" className='hidden text-lg border-b border-zinc-600 py-2' />
+          <div id="tailwindInitializer" className='hidden text-lg border-b border-zinc-600 py-2 flex justify-between text-sm text-zinc-400 items-center opacity-0 hover:opacity-100 transition' />
           <div className='monkey' />
           {children}
       </body>
